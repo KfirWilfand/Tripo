ba9c0bc61c0d74383c587075067db4a09212a566
i521652 <kfir.wilfand@sap.com>
2020-03-21 18:32:09 +0200

    test

--- src/main/java/CLIStarter.java
+++ src/main/java/CLIStarter.java
@@ -4,6 +4,7 @@ public class CLIStarter {
     public static void main(String[] args) {
         DictionaryBuilder dicBuilder = new DictionaryBuilder();
         dicBuilder.create();
+
         System.out.println(dicBuilder.getDictionary());
     }
 }
